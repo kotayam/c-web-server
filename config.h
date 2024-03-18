38b0ed588e38abfd6c28618bdf892610ec24bb67
Kota Yamamoto <ykomako4.9@gmail.com>
2024-03-18 10:47:58 -0400

    http server impl

--- config.h
+++ config.h
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 
 #define PORT 8080
-#define RESPONSE_LENGTH 256
-#define SERVER_MESSAGE_LENGTH 256
+#define RESPONSE_LENGTH 1024
+#define HTTP_HEADER_LENGTH 2048
--- server.c
+++ server.c
@@ -1,7 +1,16 @@
 #include "config.h"
 
 int main() {
-    char server_message[SERVER_MESSAGE_LENGTH] = "You have reached the server!";
+    FILE *html_data = fopen("index.html", "r");
+    if (html_data == NULL) {
+        perror("failed to open index.html");
+        exit(EXIT_FAILURE);
+    }
+    char res_data[RESPONSE_LENGTH];
+    fread(res_data, sizeof(char), RESPONSE_LENGTH, html_data);
+    char http_header[HTTP_HEADER_LENGTH] = "HTTP/1.1 200 OK\r\n\n";
+    strcat(http_header, res_data);
+
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket < 0) {
         perror("failed to create socket descriptor");
@@ -21,15 +30,20 @@ int main() {
         perror("failed to listen");
         exit(EXIT_FAILURE);
     }
-    int client_socket = accept(server_socket, NULL, NULL);
-    if (client_socket < 0) {
-        perror("failed to accept client");
-        exit(EXIT_FAILURE);
-    }
-    if (send(client_socket, server_message, sizeof(server_message), 0) < 0) {
-        perror("failed to send message to client");
-        exit(EXIT_FAILURE);
+    int client_socket;
+    while(1) {
+        client_socket = accept(server_socket, NULL, NULL);
+        if (client_socket < 0) {
+            perror("failed to accept client");
+            exit(EXIT_FAILURE);
+        }
+        if (send(client_socket, http_header, sizeof(http_header), 0) < 0) {
+            perror("failed to send http header to client");
+            exit(EXIT_FAILURE);
+        }
+        close(client_socket);
     }
     close(server_socket);
+    fclose(html_data);
     return EXIT_SUCCESS;
 }
