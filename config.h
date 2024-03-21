72901e6ffbf0aa552c090798e303e474f5b517ed
Kota Yamamoto <ykomako4.9@gmail.com>
2024-03-21 09:04:17 -0400

    load file page impl

--- config.h
+++ config.h
@@ -8,10 +8,10 @@
 #include <arpa/inet.h>
 
 #define PORT 8080
-#define SERVER_RESPONSE_LENGTH 1024
+#define SERVER_RESPONSE_LENGTH 2048
+#define SERVER_RESPONSE_DATA_LENGTH 1024
 #define CLIENT_RESPONSE_LENGTH 4096
 #define CLIENT_REQUEST_LENGTH 256
-#define HTTP_HEADER_LENGTH 2048
 #define PATH_LENGTH 128
 
 typedef struct {
@@ -25,3 +25,7 @@ void handle_request(int client_socket, file_arr *files);
 void render_html(char *res_data, char *method, char* path, file_arr *files);
 // render index.html with dynamic content
 void render_index(char *res_data, file_arr *files);
+// render file.html with dynamic content
+void render_file(char *res_data, char* sub_path);
+// free files array
+void free_files(file_arr *files);
--- server.c
+++ server.c
@@ -1,5 +1,8 @@
 #include "config.h"
 
+// get substring of string
+char *substring(char *str, int begIdx);
+
 int main() {
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket < 0) {
@@ -32,58 +35,70 @@ int main() {
         int client_socket = accept(server_socket, NULL, NULL);
         if (client_socket < 0) {
             perror("failed to accept client");
+            free_files(files);
             exit(EXIT_FAILURE);
         }
         handle_request(client_socket, files);
         close(client_socket);
     }
     close(server_socket);
-    free(files);
+    free_files(files);
     return EXIT_SUCCESS;
 }
 
 void handle_request(int client_socket, file_arr *files) {
     FILE *client_file = fdopen(client_socket, "r");
     char client_request[CLIENT_REQUEST_LENGTH];
-    if (fgets(client_request, CLIENT_REQUEST_LENGTH - 1, client_file) == NULL) {
+    if (fgets(client_request, CLIENT_REQUEST_LENGTH - 1, client_file) == 0) {
         perror("failed to read request from client");
         exit(EXIT_FAILURE);
     }
+    // fprintf(stderr, "request from client:\n%s\n", client_request);
     char *delimeter = " \n\r\t";
     char *method = strtok(client_request, delimeter);
-    char path[PATH_LENGTH] = ".";
-    char *path_r = strtok(NULL, delimeter);
-    strcat(path, path_r);
-    char http_header[HTTP_HEADER_LENGTH] = "HTTP/1.1 200 OK\r\n\n";
-    if (strcmp(path, "./favicon.ico") != 0) {
-        char *res_data = calloc(SERVER_RESPONSE_LENGTH, sizeof(char));
+    char *path = strtok(NULL, delimeter);
+    char *response = calloc(SERVER_RESPONSE_LENGTH, sizeof(char));
+    strcpy(response, "HTTP/1.1 200 OK\r\n\n");
+    if (strcmp(path, "/favicon.ico") == 0) {
+        free(response);
+        fclose(client_file);
+        return;
+    }
+    else {
+        char *res_data = calloc(SERVER_RESPONSE_DATA_LENGTH, sizeof(char));
         if (res_data == NULL) {
             perror("failed to allocate memory for response data");
             exit(EXIT_FAILURE);
         }
         render_html(res_data, method, path, files);
-        strcat(http_header, res_data);
+        strcat(response, res_data);
         free(res_data);
     }
-    if (send(client_socket, http_header, sizeof(http_header), 0) < 0) {
+    // fprintf(stderr, "response:\n%s\n", response);
+    if (send(client_socket, response, SERVER_RESPONSE_LENGTH, 0) < 0) {
         perror("failed to send http header to client");
         exit(EXIT_FAILURE);
     }
+    free(response);
     fclose(client_file);
 }
 
 void render_html(char *res_data, char *method, char *path, file_arr *files) {
     FILE *html_data;
     if (strcmp(method, "GET") == 0) {
-        if (strcmp(path, "./") == 0) {
+        if (strcmp(path, "/") == 0) {
             render_index(res_data, files);
             return;
         }
         else {
-            strtok(path, "?");
+            char *sub_path = strtok(path, "?");
+            if (strcmp(sub_path, "/add") != 0) {
+                render_file(res_data, sub_path);
+                return;
+            }
             char *key = strtok(NULL, "=");
             if (key == NULL || strcmp(key, "filename") != 0) {
-                html_data = fopen(path, "r");
+                html_data = fopen("./template/notfound.html", "r");
             }
             else {
                 char *value = strtok(NULL, "");
@@ -104,26 +119,57 @@ void render_html(char *res_data, char *method, char *path, file_arr *files) {
         perror("failed to open file");
         html_data = fopen("./template/notfound.html", "r");
     }
-    fread(res_data, sizeof(char), SERVER_RESPONSE_LENGTH - 1, html_data);
+    size_t res_size = fread(res_data, sizeof(char), SERVER_RESPONSE_DATA_LENGTH, html_data);
+    res_data[res_size] = '\0';
     fclose(html_data);
 }
 
 void render_index(char *res_data, file_arr *files) {
-    char head_buff[SERVER_RESPONSE_LENGTH];
-    FILE* head_data = fopen("index-head.html", "r");
-    fread(head_buff, sizeof(char), SERVER_RESPONSE_LENGTH - 1, head_data);
-    char foot_buff[SERVER_RESPONSE_LENGTH];
-    FILE* foot_data = fopen("index-foot.html", "r");
-    fread(foot_buff, sizeof(char), SERVER_RESPONSE_LENGTH - 1, foot_data);
-    strcat(res_data, head_buff);
+    char index_hd_buff[SERVER_RESPONSE_DATA_LENGTH];
+    FILE* index_hd_data = fopen("index-head.html", "r");
+    size_t ihd_size = fread(index_hd_buff, sizeof(char), SERVER_RESPONSE_DATA_LENGTH - 1, index_hd_data);
+    index_hd_buff[ihd_size] = '\0';
+    // fprintf(stderr, "head: %s\n", index_hd_buff);
+    char index_ft_buff[SERVER_RESPONSE_DATA_LENGTH];
+    FILE* index_ft_data = fopen("index-foot.html", "r");
+    size_t ift_size = fread(index_ft_buff, sizeof(char), SERVER_RESPONSE_DATA_LENGTH - 1, index_ft_data);
+    index_ft_buff[ift_size] = '\0';
+    // fprintf(stderr, "foot: %s\n", index_ft_buff);
+    strcat(res_data, index_hd_buff);
     for (int i = 0; i < files->num_files; i++) {
-        char *open_tag = "<li>";
-        char *close_tag = "</li>";
-        strcat(res_data, open_tag);
+        char *open_tag_hd = "<li><a href=\"";
+        char *open_tag_tl = "\">";
+        char *close_tag = "</a></li>";
+        strcat(res_data, open_tag_hd);
+        strcat(res_data, files->files[i]);
+        strcat(res_data, open_tag_tl);
         strcat(res_data, files->files[i]);
         strcat(res_data, close_tag);
     }
-    strcat(res_data, foot_buff);
-    fclose(head_data);
-    fclose(foot_data);
+    strcat(res_data, index_ft_buff);
+    fclose(index_hd_data);
+    fclose(index_ft_data);
+}
+
+void render_file(char *res_data, char *sub_path) {
+    char file_hd_buff[SERVER_RESPONSE_DATA_LENGTH];
+    FILE* file_hd_data = fopen("./template/file-head.html", "r");
+    size_t fhd_size = fread(file_hd_buff, sizeof(char), SERVER_RESPONSE_DATA_LENGTH - 1, file_hd_data);
+    file_hd_buff[fhd_size] = '\0';
+    char file_ft_buff[SERVER_RESPONSE_DATA_LENGTH];
+    FILE* file_ft_data = fopen("./template/file-foot.html", "r");
+    size_t fft_size = fread(file_ft_buff, sizeof(char), SERVER_RESPONSE_DATA_LENGTH - 1, file_ft_data);
+    file_ft_buff[fft_size] = '\0';
+    strcat(res_data, file_hd_buff);
+    strcat(res_data, file_ft_buff);
+    fclose(file_hd_data);
+    fclose(file_ft_data);
+}
+
+void free_files(file_arr *files) {
+    for (int i = 0; i < files->num_files; i++) {
+        free(files->files[i]);
+    }
+    free(files->files);
+    free(files);
 }
